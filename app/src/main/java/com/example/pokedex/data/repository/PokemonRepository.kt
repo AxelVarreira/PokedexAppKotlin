81acdb129ec73e6667d789317010dbc1baa3f542
AxelVarreira <axel.felix.varreira2@gmail.com>
2021-08-03 14:47:09 -0300

    : Nao funcional, estudar ViewAdapter, execute(), enqueue e RecyclerView, também seria bom estudar lifeCycle mais afundo.

--- app/src/main/java/com/example/pokedex/data/repository/PokemonRepository.kt
+++ app/src/main/java/com/example/pokedex/data/repository/PokemonRepository.kt
@@ -4,12 +4,12 @@ import android.app.Activity
 import android.content.Context
 import android.view.View
 import android.widget.Button
-import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import com.example.pokedex.R
 import com.example.pokedex.data.model.PokemonApiResponse
 import com.example.pokedex.data.model.PokemonDetailModel
+import com.example.pokedex.data.model.PokemonModel
 import com.example.pokedex.data.remote.PokeApiService
 import com.example.pokedex.utils.NetworkUtils
 import retrofit2.Call
@@ -18,24 +18,44 @@ import retrofit2.Response
 
 class PokemonRepository {
 
-    fun getAllPokemons(baseContext: Context, button: Button) {
+    fun getAllPokemons(baseContext: Context) : ArrayList<PokemonModel> {
         val retrofitClient = NetworkUtils
             .getRetrofitInstance()
 
         val endpoint = retrofitClient.create(PokeApiService::class.java)
-        val callback = endpoint.getAllPokemons()
 
-        callback.enqueue(object : Callback<PokemonApiResponse> {
-            override fun onFailure(call: Call<PokemonApiResponse>, t: Throwable) {
-                Toast.makeText(baseContext, t.message, Toast.LENGTH_SHORT).show()
-            }
+        // val callback = endpoint.getAllPokemons()
 
-            override fun onResponse(call: Call<PokemonApiResponse>, response: Response<PokemonApiResponse>) {
-                response.body()?.results?.forEach {
-                    button.text = button.text.toString().plus(it.name)
+        val pokeList = ArrayList<PokemonModel>()
+        val call: Call<PokemonApiResponse> = endpoint.getAllPokemons()
+
+        try {
+            val response: Response<PokemonApiResponse> = call.execute()
+
+            val pokeResponse: PokemonApiResponse? = response.body()
+
+            if(pokeResponse!==null)
+                for (pokemon in pokeResponse.results) {
+                    pokeList.add(pokemon)
                 }
-            }
-        })
+
+        }catch (e: Exception){
+            println("Erro:"+e)
+        }
+
+        // callback.enqueue(object : Callback<PokemonApiResponse> {
+        //     override fun onFailure(call: Call<PokemonApiResponse>, t: Throwable) {
+        //         Toast.makeText(baseContext, t.message, Toast.LENGTH_SHORT).show()
+        //     }
+
+        //     override fun onResponse(call: Call<PokemonApiResponse>, response: Response<PokemonApiResponse>) {
+        //         response.body()?.results?.forEach {
+        //             pokeList.add(it)
+        //         }
+        //     }
+        // })
+
+        return pokeList
     }
 
     fun getDetailedPokemon(nomePokemon: String): Response<PokemonDetailModel> {
--- app/src/main/java/com/example/pokedex/ui/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/ui/MainActivity.kt
@@ -1,21 +1,37 @@
 package com.example.pokedex.ui
 
+import android.os.AsyncTask
 import android.os.Bundle
-import android.widget.Button
-import android.widget.LinearLayout
+import android.view.View
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.pokedex.R
 import com.example.pokedex.data.repository.PokemonRepository
+
+import android.widget.ArrayAdapter
+import com.example.pokedex.R
+
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var listView: ListView
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val pokemonRepository = PokemonRepository()
+        val listaDePokemons = pokemonRepository.getAllPokemons(baseContext)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.button)
-        val pokemonRepository = PokemonRepository()
+        val frutas = ArrayList<String>()
 
-        pokemonRepository.getAllPokemons(baseContext, button)
+        if (listaDePokemons != null) {
+            for(pokemons in listaDePokemons){
+                frutas.add(pokemons.name)
+            }
+        }
+
+        listView = findViewById(R.id.button_list_view)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, frutas)
+        listView.adapter = adapter
     }
 }

3fc644637d36aefb6d62237a16bcb8a439326937
AxelVarreira <axel.felix.varreira2@gmail.com>
2021-08-09 08:57:51 -0300

    : RecyclerView funcionando, porem, nao esta pegando chamada api assincrona de uma lista, preciso da viewmodel para tal

--- app/src/main/java/com/example/pokedex/ui/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/ui/MainActivity.kt
@@ -1,17 +1,13 @@
 package com.example.pokedex.ui
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.LinearLayout
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.pokedex.R
 import com.example.pokedex.data.repository.PokemonRepository
-import com.example.pokedex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,6 +17,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /*
+        Nao esta pegando o ActivityMainBinding
+
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(
             this,
             R.layout.activity_main
@@ -28,6 +27,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.viewmodel = viewmodel
         binding.lifecycleOwner = this
+         */
 
         val pokemonRepository = PokemonRepository()
 
